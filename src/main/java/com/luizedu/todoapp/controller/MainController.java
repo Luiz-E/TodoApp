f8a3d8639daed68bbd6debb9cb484c11b6d69f94
Luiz_E <luizedumartins21@gmail.com>
2023-06-29 22:08:30 -0300

    MainScreen finished

--- src/main/java/com/luizedu/todoapp/controller/MainController.java
+++ src/main/java/com/luizedu/todoapp/controller/MainController.java
@@ -5,6 +5,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 
 import java.net.URL;
 import java.util.Objects;
@@ -15,10 +19,54 @@ public class MainController implements Initializable {
     @FXML
     public Label toolBarTitle;
     public ImageView tickView;
+    public GridPane gridPane;
+    public ImageView addProject;
+    public ImageView addTask;
 
+    private AddProjectController addProjectController;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        setTickView();
+        setGridColumnConstraints();
+        setGridRowConstraints();
+        setAddImages();
+
+        addProjectController = new AddProjectController();
+        handleAddProject();
+    }
+    @FXML
+    private void handleAddProject() {
+        if (addProjectController != null) {
+            addProjectController.init();
+        }
+    }
+
+    private void setAddImages() {
+        Image add = new Image(Objects.requireNonNull(MainController.class.getResource("/images/add.png")).toExternalForm());
+        addProject.setImage(add);
+        addTask.setImage(add);
+    }
+
+    private void setGridRowConstraints() {
+        RowConstraints rowAddItems = new RowConstraints();
+        rowAddItems.setPrefHeight(50);
+        rowAddItems.setMinHeight(50);
+        gridPane.getRowConstraints().add(rowAddItems);
+
+        RowConstraints rowListItems = new RowConstraints();
+        rowListItems.setVgrow(Priority.ALWAYS);
+        gridPane.getRowConstraints().add(rowListItems);
+    }
+
+    private void setGridColumnConstraints() {
+        ColumnConstraints projectsColumnConstraints = new ColumnConstraints();
+        projectsColumnConstraints.setPercentWidth(20);
+        ColumnConstraints tasksColumnsConstraints = new ColumnConstraints();
+        tasksColumnsConstraints.setPercentWidth(100);
+        gridPane.getColumnConstraints().addAll(projectsColumnConstraints, tasksColumnsConstraints);
+    }
+    private void setTickView() {
         Image tick = new Image(Objects.requireNonNull(MainController.class.getResource("/images/tick.png")).toExternalForm());
         tickView.setImage(tick);
     }
--- src/main/java/com/luizedu/todoapp/main/Main.java
+++ src/main/java/com/luizedu/todoapp/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/luizedu/todoapp/MainScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 720);
         scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("/css/main.css")).toExternalForm());
         stage.setTitle("TodoApp");
         stage.setScene(scene);
